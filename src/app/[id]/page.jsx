fb7d003760351a57ca97bff221f0f5ef08320d7d
codeinfinite19 <shahedhassan572@gmail.com>
2026-04-30 00:05:41 +0600

    learning about how to change Fav icon , meta data title and description and make seo friendly about project

--- src/app/[id]/page.jsx
+++ src/app/[id]/page.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+ export const metadata = {
+  title: "Dynamic route | Basic Next js With Shahed",
+  description: "We will learn about basic nextjs with Shahed vhi",
+};
 
-const DaynamicRouter = () => {
+const DaynamicRouter = async({params}) => {
+    const p= await params;
+    console.log(p,"params");
+
+   
     return (
         <div>
             <h1>This is dynamic router</h1>
